45713c595ac22e750269b2f1582a9dcbfc6d4320
Hotakus <553070703@qq.com>
2023-05-20 14:43:53 +0800

    feat(base): algorithms of sort

---
+++ base_algorithm/sort.c
@@ -0,0 +1,44 @@
+
+#include <strings.h>
+#include "sort.h"
+
+
+int *bubble_sort_int(int *src, uint64_t len, bool is_descent) {
+    if (src == NULL || len == 0) return NULL;
+    for (uint64_t i = 0; i < len - 1; ++i) {
+        for (int k = 0; k < len - i - 1; ++k) {
+            if (is_descent ? (src[k] < src[k + 1]) : src[k] > src[k + 1]) {
+                int t = src[k];
+                src[k] = src[k + 1];
+                src[k + 1] = t;
+            }
+        }
+    }
+    return NULL;
+}
+
+char *bubble_sort_char(char *src, bool is_descent) {
+    uint64_t len = strlen(src);
+    if (src == NULL || len == 0) return NULL;
+    for (uint64_t i = 0; i < len - 1; ++i) {
+        for (int k = 0; k < len - i - 1; ++k) {
+            if (is_descent ? (src[k] < src[k + 1]) : src[k] > src[k + 1]) {
+                char t = src[k];
+                src[k] = src[k + 1];
+                src[k + 1] = t;
+            }
+        }
+    }
+    return NULL;
+}
+
+char *bubble_sort_str_arr(char **src, bool is_descent) {
+
+    return NULL;
+}
+
+void *bubble_sort_void(void *src, bool is_descent) {
+    if (src == NULL) return NULL;
+    return NULL;
+}
+
---
+++ base_algorithm/sort.h
@@ -0,0 +1,23 @@
+#ifndef CODEWAR_KATA_C_SORT_H
+#define CODEWAR_KATA_C_SORT_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*...*/
+// bubble sort function
+int *bubble_sort_int( int *src, uint64_t len, bool is_descent);
+char *bubble_sort_char(char *src, bool is_descent);
+char *bubble_sort_str_arr(char **src, bool is_descent);
+void *bubble_sort_void(void *src, bool is_descent);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //CODEWAR_KATA_C_SORT_H
