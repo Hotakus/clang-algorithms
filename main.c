b5ec0e2970f172bcdc834dc3d03f67f7cebacef1
Hotakus <553070703@qq.com>
2023-06-11 05:59:13 +0800

    chore(main): basic test

--- main.c
+++ main.c
@@ -1,28 +1,11 @@
 #include <stdio.h>
-#include <math.h>
 #include "main.h"
-
-
-#include <inttypes.h>
-#include <stdio.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-
-#include <stdbool.h>
-#include <time.h>
-#include <ctype.h>
-
-#include <math.h>
-
 
 char *str = "hello world asd x2 21 a !";
 char *str2 = "hello";
 
 int main() {
-    // chain_test();
+    chain_test();
 
     int n = 0;
     char **list = str_split(str, ' ', &n);
