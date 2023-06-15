0e0af525c8f9420532b68746a2baa0673344ec04
hotakus <ttowfive@gmail.com>
2023-06-15 19:04:10 +0800

    chore(main): basic test

--- main.c
+++ main.c
@@ -6,7 +6,14 @@
 char *str = "hello world asd x2 21 a !";
 char *str2 = "hello";
 
+void rgb (int r, int g, int b, char *buf)
+{
+    r = r > 255 ? 255 : (r < 0 ? 0 : r);
+    g = g > 255 ? 255 : (g < 0 ? 0 : g);
+    b = b > 255 ? 255 : (b < 0 ? 0 : b);
+    sprintf(buf, "%02X%02X%02X", r, g, b);
+}
+
 int main() {
     chain_test();
-
 }
