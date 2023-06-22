0449dda7c4d074fc1968eb342c3168d7f6343c5b
hotakus <ttowfive@gmail.com>
2023-06-22 13:07:13 +0800

    feat(katas): big num multiply

--- katas/big_num.c
+++ katas/big_num.c
@@ -12,13 +12,6 @@
 #include "big_num.h"
 #include "../basic_algorithm//ba.h"
 
-/**
- * 1. determine digits who is bigger.
- * 2. align digits and allocate a array.
- * 3. add digits one by one.
- * 4. carrying digits if it bigger than 10.
- * 5. if digits len is bigger than old one, then reallocate array.
- */
 
 /**
  * 大数相加
@@ -26,10 +19,18 @@
  * @param b 大数b
  * @return 结果
  */
-char *big_num_add(const char *a, const char *b) {
+char *big_num_add(const char *num1, const char *num2) {
+    /**
+     * 1. determine digits who is bigger.
+     * 2. align digits and allocate a array.
+     * 3. add digits one by one.
+     * 4. carrying digits if it bigger than 10.
+     * 5. if digits len is bigger than old one, then reallocate array.
+     */
 
-    unsigned char len_a = BA_STRLEN(a);
-    unsigned char len_b = BA_STRLEN(b);
+    // TODO: 改进
+    unsigned char len_a = BA_STRLEN(num1);
+    unsigned char len_b = BA_STRLEN(num2);
     unsigned char len_max = MAX(len_a, len_b);
 
     char *num_buf = (char *) calloc(len_max + 1 + 1, sizeof(char));
@@ -40,9 +41,9 @@ char *big_num_add(const char *a, const char *b) {
     for (int k = 0; k < len_max; ++k) {
         int digit = carry;
         if ((len_a - k - 1) >= 0)
-            digit += a[len_a - k - 1] - '0';
+            digit += num1[len_a - k - 1] - '0';
         if ((len_b - k - 1) >= 0)
-            digit += b[len_b - k - 1] - '0';
+            digit += num2[len_b - k - 1] - '0';
 
         num_buf[lm - k] = (char) ((digit % 10) + '0');
         carry = digit / 10;
@@ -59,4 +60,74 @@ char *big_num_add(const char *a, const char *b) {
     }
 
     return num_buf;
+}
+
+
+// TODO: 实现更多大数乘方法
+char *big_num_multiply(const char *num1, const char *num2) {
+
+    num1 = num_trailing_zeros(num1);
+    num2 = num_trailing_zeros(num2);
+
+    if (num1[0] == NULL || num2[0] == NULL) {
+        char *tmp = calloc(1, 1);
+        tmp[0] = '0';
+        return tmp;
+    }
+
+    unsigned short len_1 = BA_STRLEN(num1);
+    unsigned short len_2 = BA_STRLEN(num2);
+    unsigned int   len = len_1 + len_2;
+
+    char *digits = (char *) calloc(len + 1, sizeof(char));
+
+    for (int i = len_1 - 1; i >= 0; --i) {
+        unsigned char carry = 0;
+        for (int k = len_2 - 1; k >= 0; --k) {
+            unsigned char res = (num1[i] - '0') * (num2[k] - '0');
+            carry = res / 10;
+            digits[k + i + 1] += res % 10;
+            if (carry)
+                digits[k + i] += carry;
+        }
+
+        // 进位
+        for (int k = len_2 - 1; k >= 0; --k) {
+            if (digits[k + i + 1] == 0) continue;
+            carry = digits[k + i + 1] / 10;
+            digits[k + i + 1] = digits[k + i + 1] % 10;
+            digits[k + i] += carry;
+        }
+    }
+
+    // 转数字为字符并分拷贝到新空间
+    for (int k = 0; (k < len); ++k)
+        digits[k] += '0';
+    if (digits[0] == '0') {
+        char *tmp = calloc(len + 1, sizeof(char));
+        BA_STRCPY(tmp, &digits[1]);
+        free(digits);
+        digits = tmp;
+    }
+
+    digits[len] = '\0';
+    return digits;
+}
+
+void big_num_free(char *num) {
+    if (num == NULL) {
+#if DEBUG == 1
+        printf("%s pointer is null\n", __FUNCTION__);
+#endif
+    }
+}
+
+/**
+ * 去除数字字符串中的前置trailing的0
+ * @param num
+ * @return
+ */
+char *num_trailing_zeros(char *num) {
+    while (*num++ == '0' && (num != '\0'));
+    return --num;
 }
--- katas/include/big_num.h
+++ katas/include/big_num.h
@@ -11,14 +11,19 @@
 #ifndef CODEWAR_KATA_C_BIG_NUM_H
 #define CODEWAR_KATA_C_BIG_NUM_H
 
-
+#define BIG_NUM_MUL_USE_NORMAL      1
+#define BIG_NUM_MUL_USE_KARATSUBA   0
+#define BIG_NUM_MUL_USE_FNTT        0
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-char *big_num_add(const char *a, const char *b);
+char *num_trailing_zeros(char *num);
 
+char *big_num_add(const char *num1, const char *num2);
+char *big_num_multiply(const char *num1, const char *num2);
+void big_num_free(char *num);
 
 #ifdef __cplusplus
 }
