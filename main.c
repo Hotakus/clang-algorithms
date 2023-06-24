a763743fa62d404744f3ea67d2057165306a6962
hotakus <ttowfive@gmail.com>
2023-06-24 08:54:50 +0800

    test(main): create test func

--- main.c
+++ main.c
@@ -11,25 +11,32 @@ char *str1 = "pqmpgtzzfaqftdawjqiurwscy";
 char *str2 = "fevbekrsaanbbsbrzwwbvfsdb";
 char *ex = "2:bbbbbb/1:qqq/2:sss/1:pp/1:tt/1:zz/2:ee/2:rr/2:vv/=:aa/=:ff/=:ww";
 
+void fun_test() {
+    // sort test
+    printf("------------------ Sort Test (Begin) ------------------\n");
+    char *test_s1 = calloc(BA_STRLEN(str1), sizeof(char));
+    BA_STRCPY(test_s1, str1);
+    bubble_sort_char(test_s1, false);
+    printf("%s\n", test_s1);
+    free(test_s1);
+    printf("------------------ Sort Test ( End ) ------------------\n\n");
 
-static int tf1(int a, int b) {
-    return a + b;
-}
-
-static int tf2(int a, int b) {
-    return a * b;
-}
-
-
-typedef int (*test_func_t)(int a, int b);
-
-typedef struct {
-    test_func_t tf;
-}test_struct_t;
-
-int main() {
+    // chain test
+    printf("------------------ Chain Test (Begin) ------------------\n");
     long long int cost_time = run_time(COUNT_TIME_US, chain_test);
     run_time_show(&cost_time);
     printf("%s\n", CW_KATA_VER);
+    printf("------------------ Chain Test ( End ) ------------------\n\n");
+
+    // string mix
+    printf("------------------ String Mix (Begin) ------------------\n");
+    char *res1 = string_mix(str1, str2);
+    printf("%s\n", res1);
+    free(res1);
+    printf("------------------ String Mix ( End ) ------------------\n\n");
+}
+
+int main() {
+    fun_test();
     return 0;
 }
